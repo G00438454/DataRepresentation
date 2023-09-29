b3052ad8ed4c609ec66f2cf047490fe0d9a0e789
G00438454 <g00438454@atu.ie>
2023-09-29 09:47:33 +0000

    ex2

---
+++ ex2pt2.js
@@ -0,0 +1,8 @@
+// control + k, control + c (comment)
+// control + k, control + u (undo the comment)
+// alt + shift + f (rearrange)
+let part2b = (myValue) => {
+    return myValue;
+}
+
+console.log(part2b("hello"));
