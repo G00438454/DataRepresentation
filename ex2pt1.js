bf79785904334e0edd8648196bc0f4600b75cfe2
G00438454 <g00438454@atu.ie>
2023-09-29 09:43:13 +0000

    ex2

---
+++ ex2pt1.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+
+const SayHi = function Hello() {
+    console.log("Hi");
+}
+
+SayHi();
+
+let sayBye = () => {
+    console.log("Bye");
+}
+
+sayBye();
+
+let test = (myValue) => {
+    console.log(myValue);
+}
+
+test("Bonjour");
+
+
+let DataRep = () => {
+    console.log("Data Representation & Querying");
+}
+
+DataRep();
+
+
+
